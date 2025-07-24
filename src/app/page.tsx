4c2a258585f916f816c486899e46e391aabfe399
It Solution <najalahi9@gmail.com>
2025-07-24 17:42:01 +0000

    its not

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -280,7 +280,7 @@ export default function DashboardPage() {
         </Card>
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+      <div className="grid grid-cols-1 gap-8">
         <Card>
           <CardHeader>
             <CardTitle>Upcoming Matches</CardTitle>
