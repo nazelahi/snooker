811da0d5a974ba11a1783e4af4a7d5d5929d8a1e
It Solution <najalahi9@gmail.com>
2025-07-24 10:42:47 +0000

    remove arrow key from carousel

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -86,19 +86,6 @@ const Carousel = React.forwardRef<
       api?.scrollNext()
     }, [api])
 
-    const handleKeyDown = React.useCallback(
-      (event: React.KeyboardEvent<HTMLDivElement>) => {
-        if (event.key === "ArrowLeft") {
-          event.preventDefault()
-          scrollPrev()
-        } else if (event.key === "ArrowRight") {
-          event.preventDefault()
-          scrollNext()
-        }
-      },
-      [scrollPrev, scrollNext]
-    )
-
     React.useEffect(() => {
       if (!api || !setApi) {
         return
