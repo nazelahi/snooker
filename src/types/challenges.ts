01e4f305167b6dddd7e006f5f6f1ad5cc19d3ae7
It Solution <najalahi9@gmail.com>
2025-07-27 08:13:11 +0000

    find any error in my project that prevent building in netlify

--- src/types/challenges.ts
+++ src/types/challenges.ts
@@ -1,8 +1,2 @@
 
-export interface Challenge {
-  id: string;
-  challengerEmail: string;
-  challengedEmail: string;
-  status: 'pending' | 'accepted' | 'declined';
-  date: string; // ISO date string of when challenge was made
-}
+    
